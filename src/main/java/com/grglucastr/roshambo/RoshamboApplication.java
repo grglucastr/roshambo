3a8a8596b0b24357c0703257fdad823c95907ca4
George Bentes <george.bentes@gmail.com>
2020-06-29 21:52:13 -0300

    Add initial files

---
+++ src/main/java/com/grglucastr/roshambo/RoshamboApplication.java
@@ -0,0 +1,13 @@
+package com.grglucastr.roshambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshamboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshamboApplication.class, args);
+	}
+
+}
