fda7f1c1ba021dfba1631c4085a4a9c4bad72610
George Bentes <george.bentes@gmail.com>
2020-07-02 15:16:08 -0300

    Refactor endpoints names

--- src/main/java/com/grglucastr/roshambo/controller/v1/PlayerController.java
+++ src/main/java/com/grglucastr/roshambo/controller/v1/PlayerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/players")
 public class PlayerController {
 
     private PlayerRepository playerRepository;
@@ -22,12 +22,12 @@ public class PlayerController {
         this.playerRepository = playerRepository;
     }
 
-    @GetMapping(value = {"/players"},  produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Player>>listAllPlayers(){
         return ResponseEntity.ok(playerRepository.listAll());
     }
 
-    @GetMapping(value = {"/players/{username}"},  produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{username}",  produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Player> getPlayer(@PathVariable("username") String username){
         Optional<Player> player = playerRepository.findByUsername(username);
 
@@ -35,13 +35,13 @@ public class PlayerController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping(value = {"/players"}, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Player> addNewPlayer(@RequestBody Player newPlayer){
         Player player = playerRepository.add(newPlayer);
         return ResponseEntity.status(HttpStatus.CREATED).body(player);
     }
 
-    @PutMapping(value = {"/players/{username}"}, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(value = "/{username}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Player> update(@PathVariable("username")String username, @RequestBody Player player){
         Optional<Player> foundPlayer = playerRepository.findByUsername(username);
 
@@ -56,7 +56,7 @@ public class PlayerController {
         return ResponseEntity.ok(updatePlayer);
     }
 
-    @DeleteMapping( value = {"/players/{id}"})
+    @DeleteMapping( value = "/{id}")
     public ResponseEntity deletePlayer(@PathVariable("id") Integer id){
         if(playerRepository.removeById(id)){
             return ResponseEntity.noContent().build();
