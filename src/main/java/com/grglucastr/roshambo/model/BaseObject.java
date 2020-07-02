4b562a423b1debe0adade22551106c8be3d7da6f
George Bentes <george.bentes@gmail.com>
2020-07-02 00:39:00 -0300

    Add base class for model business objects

---
+++ src/main/java/com/grglucastr/roshambo/model/BaseObject.java
@@ -0,0 +1,33 @@
+package com.grglucastr.roshambo.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public abstract class BaseObject implements Serializable {
+    private Integer id;
+
+    public BaseObject(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseObject that = (BaseObject) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
