f5b2c2fbe26f2f66fb5ac447ce052f3571908754
George Bentes <george.bentes@gmail.com>
2020-07-02 15:01:55 -0300

    Add extension to hash map to avoid add the same strategy as weakness or strength

---
+++ src/main/java/com/grglucastr/roshambo/model/SetStrategy.java
@@ -0,0 +1,18 @@
+package com.grglucastr.roshambo.model;
+import java.util.HashSet;
+
+public class SetStrategy extends HashSet<Strategy> {
+
+    private Strategy parent;
+
+    public SetStrategy(Strategy parent){
+        this.parent = parent;
+    }
+
+    public boolean add(Strategy strategy) {
+        if(parent.equals(strategy)){
+            return false;
+        }
+        return super.add(strategy);
+    }
+}
