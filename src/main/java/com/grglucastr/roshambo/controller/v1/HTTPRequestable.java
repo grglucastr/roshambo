6bb6b316c9e351b7f3d8412eeac98b0149140d5f
George Bentes <george.bentes@gmail.com>
2020-07-03 12:02:29 -0300

    Remove addNew from interface contract

--- src/main/java/com/grglucastr/roshambo/controller/v1/HTTPRequestable.java
+++ src/main/java/com/grglucastr/roshambo/controller/v1/HTTPRequestable.java
@@ -8,7 +8,6 @@ public interface HTTPRequestable<T> {
 
     ResponseEntity<List<T>> listAll();
     ResponseEntity<T> getSingleOne(Integer id);
-    ResponseEntity<T> addNew(T newObj);
     ResponseEntity<T> update(Integer id, T obj);
     ResponseEntity delete(Integer id);
 }
