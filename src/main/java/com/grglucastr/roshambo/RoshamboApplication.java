d9e90dac30797f756fbb1c69acc07a295a91d587
George Bentes <george.bentes@gmail.com>
2020-07-02 21:47:08 -0300

    Add Players and Moves seeding

--- src/main/java/com/grglucastr/roshambo/RoshamboApplication.java
+++ src/main/java/com/grglucastr/roshambo/RoshamboApplication.java
@@ -1,6 +1,10 @@
 package com.grglucastr.roshambo;
 
+import com.grglucastr.roshambo.model.Move;
+import com.grglucastr.roshambo.model.Player;
 import com.grglucastr.roshambo.model.Strategy;
+import com.grglucastr.roshambo.repository.v1.MoveRepository;
+import com.grglucastr.roshambo.repository.v1.PlayerRepository;
 import com.grglucastr.roshambo.repository.v1.StrategyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +12,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class RoshamboApplication implements CommandLineRunner {
 
 	@Autowired
 	private StrategyRepository strategyRepository;
+
+	@Autowired
+	private PlayerRepository playerRepository;
+
+	@Autowired
+	private MoveRepository moveRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoshamboApplication.class, args);
@@ -25,6 +37,14 @@ public class RoshamboApplication implements CommandLineRunner {
 
 		getStrategies().forEach(strategy -> {
 			strategyRepository.add(strategy);
+		});
+
+		getPlayers().forEach(player ->{
+			playerRepository.add(player);
+		});
+
+		getMoves().forEach(move -> {
+			moveRepository.add(move);
 		});
 	}
 
@@ -58,5 +78,24 @@ public class RoshamboApplication implements CommandLineRunner {
 		lizard.setMultipleWeaknesses(rock, scissor);
 
 		return Arrays.asList(rock, paper, scissor, spock, lizard);
+	}
+
+	public List<Player> getPlayers(){
+		List<Player> players = Arrays.asList(
+				new Player(1, "Player1"),
+				new Player(2, "Player2"),
+				new Player(3, "Player3"),
+				new Player(4, "Player4")
+		);
+		return players;
+	}
+
+	public Set<Move> getMoves(){
+		Set<Move> moves = new HashSet<>();
+		moves.add(new Move(1, getPlayers().get(0), getStrategies().get(0)));
+		moves.add(new Move(2, getPlayers().get(1), getStrategies().get(1)));
+		moves.add(new Move(3, getPlayers().get(2), getStrategies().get(2)));
+		moves.add(new Move(4, getPlayers().get(3), getStrategies().get(3)));
+		return moves;
 	}
 }
